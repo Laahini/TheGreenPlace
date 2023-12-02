8e70d1f0574ef8df05c8b0887aca034ed0fce99d
Laahini <62526180+Laahini@users.noreply.github.com>
2023-12-02 15:09:27 -0500

    update currentjobs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,3 +5,12 @@ $(".checkbox-dropdown").click(function () {
 $(".checkbox-dropdown ul").click(function (e) {
   e.stopPropagation();
 });
+
+function showMore() {
+  var els = document.getElementsByClassName('hidden');
+  while (els[0]) {
+    els[0].classList.remove('hidden');
+  }
+  var button = document.getElementById('showMoreBtn');
+  button.classList.add('hidden');
+}
