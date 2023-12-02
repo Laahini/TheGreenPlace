4a07e4ca9dabc4c8c6f7a4ac09146e6e4bec0fa8
Laahini <62526180+Laahini@users.noreply.github.com>
2023-12-02 10:25:09 -0500

    update careers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,16 +5,3 @@ $(".checkbox-dropdown").click(function () {
 $(".checkbox-dropdown ul").click(function (e) {
   e.stopPropagation();
 });
-
-
-// Simulation controls to illustrate differences
-// This is not relevant to the implementing the fanciness
-const element = document.querySelector('main')
-// slider communication
-const setProperty = (property, value) => document.body.style.setProperty(property, value)
-// fanciness on/off
-const toggleTheFancy = () => element.classList.toggle('fancy')
-// shape shifting
-const toggleTheShape = () => element.classList.toggle('square')
-
-setProperty('--demo-width', 100)
