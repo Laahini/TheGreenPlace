991b67c875f08b4318f28dde3fd5315ddda935b1
Laahini <62526180+Laahini@users.noreply.github.com>
2023-12-02 17:47:35 -0500

    finish basic jobs page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,4 +13,69 @@ function showMore() {
   }
   var button = document.getElementById('showMoreBtn');
   button.classList.add('hidden');
+  document.getElementById('shownMessage').innerHTML="Showing all available jobs";
 }
+
+
+
+
+/*JOB SEARCH FILTERS*/
+var allCheckboxes = document.querySelectorAll('input[type=checkbox]');
+var allPlayers = Array.from(document.querySelectorAll('.jobCard'));
+var checked = {};
+
+getChecked('careerType');
+getChecked('location');
+getChecked('jobType');
+
+
+Array.prototype.forEach.call(allCheckboxes, function (el) {
+  el.addEventListener('change', toggleCheckbox);
+});
+
+function toggleCheckbox(e) {
+  getChecked(e.target.name);
+  setVisibility();
+}
+
+function getChecked(name) {
+  checked[name] = Array.from(document.querySelectorAll('input[name=' + name + ']:checked')).map(function (el) {
+    return el.value;
+  });
+}
+
+function setVisibility() {
+  allPlayers.map(function (el) {
+    var careerType = checked.careerType.length ? _.intersection(Array.from(el.classList), checked.careerType).length : true;
+    var location = checked.location.length ? _.intersection(Array.from(el.classList), checked.location).length : true;
+    var jobType = checked.jobType.length ? _.intersection(Array.from(el.classList), checked.jobType).length : true;
+    if (careerType && location && jobType) {
+      el.style.display = 'block';
+    } else {
+      el.style.display = 'none';
+    }
+  });
+}
+
+
+
+/*CONTACT FORM*/
+function ValidateEmail()
+{
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  var email = document.getElementById('emailInput');
+  var message = document.getElementById('submitted');
+  if(email.value.match(mailformat))
+  {
+    location.href="index.html";
+    return true;
+  }
+  else
+  {
+    submitted.innerHTML="Invalid email address";
+    return false;
+  }
+}
+
+
+/*JOB APPLICATION*/
